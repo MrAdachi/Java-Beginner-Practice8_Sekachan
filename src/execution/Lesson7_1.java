b42890c890f8c23a7b68b817dbd2d12158e25569
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-05-08 18:06:31 +0900

    Lesson7-1@Stringクラス（改善策）

--- src/execution/Lesson7_1.java
+++ src/execution/Lesson7_1.java
@@ -6,14 +6,15 @@ public class Lesson7_1 {
 		String[] names = {"桜井", "田原", "中川", "鈴木"};
 		String search = args[0];
 		boolean result = false; // 検索結果を格納
+		int i;
 		
-		for(int i = 0; i < names.length; i++) {
+		for(i = 0; i < names.length; i++) {
 			if(args[0].equals(names[i])) {
-				result = true;
+				break;
 			}
 		}
 		
-		if(result) {
+		if(i < names.length) {
 			System.out.println(search + "さんは含まれています");
 		} else {
 			System.out.println(search + "さんは含まれていません");
